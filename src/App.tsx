bcf6863dd4d6b33209ed854d8aabb99f9a95575c
Pavel D <pvdanilov88@inbox.ru>
2024-02-18 21:23:20 +0300

    начало

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="header-arrow">
+          <img src="img/стрелкавлево.png" alt="arrow" />
+        </div>
+        <div className="header-block">
+          <div className="header-title">
+            <p className="header-title-text">
+              ST<span className="header-ampersant"> & </span>L
+            </p>
+          </div>
+          <div className="header-personal">
+            <p>Личный кабинет</p>
+            <img src="img/стрелкавниз.svg" alt="arrow" />
+          </div>
+        </div>
       </header>
+      <main className="main">
+        <nav className="nav">
+          <div className="nav-icon">
+            <img src="img/иконкаменю.svg" alt="" />
+          </div>
+          <div className="nav-icon">
+            <img src="img/иконкаменю.svg" alt="" />
+          </div>
+          <div className="nav-icon">
+            <img src="img/иконкаменю.svg" alt="" />
+          </div>
+          <div className="nav-icon">
+            <img src="img/иконкаменю.svg" alt="" />
+          </div>
+          <div className="nav-icon">
+            <img src="img/иконкаменю.svg" alt="" />
+          </div>
+          <div className="nav-icon">
+            <img src="img/иконкаменю.svg" alt="" />
+          </div>
+
+        </nav>
+        <nav className="nav-text">
+          <div className="nav-text-item nav-text-item-orange">
+            <p>Цепочка поставок</p>
+          </div>
+          <div className="nav-text-item nav-text-item-change">
+            <p>Оперативная работа</p>
+          </div>
+          <div className="nav-text-item">
+            <p>Архив</p>
+          </div>
+          <div className="nav-text-item">
+            <p>Аналитика</p>
+          </div>
+          <div className="nav-text-item">
+            <p>Настройки</p>
+          </div>
+
+        </nav>
+        <div className="main-content">
+          <footer className="footer"></footer>
+        </div>
+      </main>
     </div>
   );
 }
